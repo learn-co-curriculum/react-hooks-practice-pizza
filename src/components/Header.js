ccef174518803ca2e964734bc26f00ae5470f629
HyeokJungKim <31145202+HyeokJungKim@users.noreply.github.com>
2019-05-30 09:10:30 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
   render() {
     return (
       <Fragment>
-        <h1 className="text-center">Welcome to Access Lab's Pizzeria</h1>
+        <h1 className="text-center">Welcome to Flatiron School's Pizzeria</h1>
       </Fragment>
     );
   }
