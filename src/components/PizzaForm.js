e91568fc5d3b20d0fba0503ee051baa446d459be
ihollander <ianholla@gmail.com>
2020-12-06 18:24:28 -0500

    Updated form

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -2,47 +2,50 @@ import React from "react";
 
 function PizzaForm() {
   return (
-    <div className="form-row">
-      <div className="col-5">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Pizza Topping"
-        />
-      </div>
-      <div className="col">
-        <select className="form-control">
-          <option value="Small">Small</option>
-          <option value="Medium">Medium</option>
-          <option value="Large">Large</option>
-        </select>
-      </div>
-      <div className="col">
-        <div className="form-check">
+    <form onSubmit={null /*handle that submit*/}>
+      <div className="form-row">
+        <div className="col-5">
           <input
-            className="form-check-input"
-            type="radio"
-            name="type"
-            value="Vegetarian"
+            className="form-control"
+            type="text"
+            name="topping"
+            placeholder="Pizza Topping"
           />
-          <label className="form-check-label">Vegetarian</label>
         </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="type"
-            value="Not Vegetarian"
-          />
-          <label className="form-check-label">Not Vegetarian</label>
+        <div className="col">
+          <select className="form-control" name="size">
+            <option value="Small">Small</option>
+            <option value="Medium">Medium</option>
+            <option value="Large">Large</option>
+          </select>
+        </div>
+        <div className="col">
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="vegetarian"
+              value="Vegetarian"
+            />
+            <label className="form-check-label">Vegetarian</label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="vegetarian"
+              value="Not Vegetarian"
+            />
+            <label className="form-check-label">Not Vegetarian</label>
+          </div>
+        </div>
+        <div className="col">
+          <button type="submit" className="btn btn-success">
+            Submit
+          </button>
         </div>
       </div>
-      <div className="col">
-        <button type="submit" className="btn btn-success" onClick={console.log}>
-          Submit
-        </button>
-      </div>
-    </div>
+    </form>
   );
 }
 
